a0f238519607784c2cb9956c9a91fd69cb3ebb53
Jae-Wook-Kim <8370kjw@naver.com>
2023-11-01 15:32:57 +0900

    register&login_use_email

--- common/models.py
+++ common/models.py
@@ -1,4 +1,51 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('이메일은 필수입니다.')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+    
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField('email address', unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name='groups',
+        blank=True,
+        help_text='The groups this user belongs to. A user will get all granted to each of their groups.',
+        related_name="customuser_set",
+        related_query_name="user",
+    )
+
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name='user permissions',
+        blank=True,
+        help_text='Speciffic permissions for this user.',
+        related_name="customuser_set",
+        related_query_name="user",
+    )
+
+    def __str__(self):
+        return self.email
 
 class User(models.Model):
     userID = models.CharField(max_length=30, primary_key=True)
--- common/views.py
+++ common/views.py
@@ -1,5 +1,6 @@
-from django.contrib import auth
-from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.contrib import auth, messages
+from django.core.exceptions import ValidationError
+from django.contrib.auth import get_user_model, authenticate, login as auth_login, logout as auth_logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
@@ -8,31 +9,64 @@ from .models import User
 
 # Create your views here.
 
+# def signup(request):
+#     if request.method == 'POST':
+#         print("Success!!")
+#         if request.POST['password1'] == request.POST['password2']:
+#             user = User.objects.create_user(
+#                 username= request.POST['email'],
+#                 password=request.POST['password1']
+#             )
+#             auth.login(request, user)
+#             return redirect('/')
+#         return render(request, '../templates/register.html')
+#     return render(request, '../templates/register.html')
+
+User = get_user_model()
+
 def signup(request):
     if request.method == 'POST':
-        print("Success!!")
-        if request.POST['password1'] == request.POST['password2']:
-            user = User.objects.create_user(
-                username= request.POST['email'],
-                password=request.POST['password1']
-            )
-            auth.login(request, user)
-            return redirect('/')
-        return render(request, '../templates/register.html')
-    return render(request, '../templates/register.html')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        
+        if password1 != password2:
+            messages.error(request, '비밀번호가 일치하지 않습니다.')
+        else:
+            try:
+                user = User.objects.create_user(email=email, password=password1)
+                user = authenticate(request, email=email, password=password1)
+                auth_login(request, user)
+                return redirect('/')
+            except ValidationError as e:
+                messages.error(request, e.message)
 
+    return render(request, '../templates/register.html')
+    
 def login(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request, data = request.POST)
-        if form.is_valid():
-            auth_login(request, form.get_user())
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request=request, email=email, password=password)
+        if user is not None:
+            auth_login(request, user)
             return redirect('/')
         else:
-            form = AuthenticationForm()
-        context = {
-            'form' : form,
-        }
-        return render(request, '../templates/login.html', context)
+            messages.error(request, '이메일 혹은 비밀번호가 올바르지 않습니다.')
+        return render(request, '../templates/login.html')
+
+# def login(request):
+#     if request.method == 'POST':
+#         form = AuthenticationForm(request, data = request.POST)
+#         if form.is_valid():
+#             auth_login(request, form.get_user())
+#             return redirect('/')
+#         else:
+#             form = AuthenticationForm()
+#         context = {
+#             'form' : form,
+#         }
+#         return render(request, '../templates/login.html', context)
     
 # @require_POST
 def logout(request):
